1a1a6868960737728d5762a3b8dc5ee6216edb5d
Pungent <ahmad.khalid.alahmad@gmail.com>
2020-11-17 17:47:57 +0300

    Added main()
    main function to take initial input, and run future functions
    
    note: we need to add another Boolean to the while loop that the sign up or log in is successful

--- Project Main.py
+++ Project Main.py
@@ -1,4 +1,20 @@
-print("Test Statement")
-print("وجهك")
-print("test")
-print("XD POOP")
+def main():
+    option = input("Please Select your option.\n(S) for sign up\n(L) for log in\n")
+    valid = False
+    while not valid:
+        if option.upper() == "L":
+            valid = True
+            print("Log-in")
+            # Log-in Function
+        elif option.upper() == "S":
+            valid = True
+            print("Sign-up")
+            # Sign-up Function
+        else:
+            valid = False
+            print("Invalid Option, please enter either S or L\n" + "#" * 43)
+            option = input("(S) for sign up\n(L) for log in\n")
+            # Repeat loop if the input is invalid
+
+
+main()
